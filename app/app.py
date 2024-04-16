2b5f53450d2dd651aa29ccdbb46ad68152d78c24
Daniel Cirino <daniel.cirino@gmail.com>
2024-04-16 19:48:32 -0300

    Alteração do título da aplicaÇÃO

--- app/app.py
+++ app/app.py
@@ -4,10 +4,11 @@ import dash_bootstrap_components as dbc
 from callbacks import configurar_callbacks
 from layout import configurar_layout
 import plotly.io as pio
+
 pio.templates.default = "plotly_dark"
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],)
-
+app = dash.Dash("smart-k-means-app", external_stylesheets=[dbc.themes.BOOTSTRAP], )
+app.title = "Smart K-Means"
 # Layout da aplicação
 configurar_layout(app)
 configurar_callbacks(app)
